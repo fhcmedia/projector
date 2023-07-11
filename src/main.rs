c546c6ee5675df23cef6222fe6c2b245e7912a75
timmypidashev <pidashev.tim@gmail.com>
2023-07-11 11:17:19 -0700

    Match project name in main.rs to projector

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use platform::{AbstractApplication, AbstractWindow, Application, Window};
 fn main() {
     // Create application
     let app = Application::create()
-        .application_id("com.fhcc.projector");
+        .application_id("com.projector");
 
     // Create window
     let window = Window::create()
         .default_width(400)
         .default_height(400)
-        .title("FHCC Projector");
+        .title("Projector");
 
     // NOTE: Any windows/tabs/lists/any content will follow 
     // the same style of creation as 'app' and 'window'.
